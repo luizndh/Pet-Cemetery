ce9576bdf293f67d79f62b5f3dcfd2f829d4f132
luiz <luiz.ndh@gmail.com>
2025-03-24 23:08:53 -0300

    bootstrap 5 adicionado / navbar finalizada / pagina inicial incompleta

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PaginaInicialComponent
+    },
+
+];
---
+++ front-end/src/app/pagina-inicial/pagina-inicial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-inicial',
+  imports: [],
+  templateUrl: './pagina-inicial.component.html',
+  styleUrl: './pagina-inicial.component.scss'
+})
+export class PaginaInicialComponent {
+
+}
