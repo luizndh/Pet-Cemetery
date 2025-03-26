f51a5615518764ae9a9627a8cf41e255d10cfe7e
luiz <luiz.ndh@gmail.com>
2025-03-26 00:01:17 -0300

    telas de informacoes da navbar e pagina inicial finalizada

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,10 +1,41 @@
 import { Routes } from '@angular/router';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CadastroComponent } from './auth/cadastro/cadastro.component';
+import { QuemSomosComponent } from './info-navbar/quem-somos/quem-somos.component';
+import { PlanosComponent } from './info-navbar/planos/planos.component';
+import { ContatoComponent } from './info-navbar/contato/contato.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: PaginaInicialComponent
     },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+    {
+        path: 'quem-somos',
+        component: QuemSomosComponent
+    },
+    {
+        path: 'planos',
+        component: PlanosComponent
+    },
+    {
+        path: 'contato',
+        component: ContatoComponent
+    },
+
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
 
 ];
---
+++ front-end/src/app/info-navbar/planos/planos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planos',
+  imports: [],
+  templateUrl: './planos.component.html',
+  styleUrl: './planos.component.scss'
+})
+export class PlanosComponent {
+
+}
