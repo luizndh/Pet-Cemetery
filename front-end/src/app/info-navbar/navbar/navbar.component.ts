8c081f524e02687775f5dd906ed55dfdc83ab9b9
luiz <luiz.ndh@gmail.com>
2025-04-29 19:34:05 -0300

    navbar atualizando quando usuario loga/desloga

--- front-end/src/app/info-navbar/navbar/navbar.component.ts
+++ front-end/src/app/info-navbar/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { LocalStorageService } from '../../shared/service/local-storage.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+    usuarioLogado: boolean = false;
+
+    constructor(private tokenService: LocalStorageService, private router: Router) { }
+
+    ngOnInit(): void {
+        this.usuarioLogado = this.tokenService.hasToken();
+    }
+
+    logout() {
+        this.tokenService.removeToken();
+        this.usuarioLogado = false;
+        this.router.navigate(['/']);
+    }
 }
